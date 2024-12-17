bd27c3131deca0c4596bafa576ecd12d92141cf4
niyonagize6369 <niyonagizerachel10@gmail.com>
2024-12-17 15:39:27 +0530

    ready to push

---
+++ src/component/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+function home() {
+return(
+<div>
+<h2> Development programming</h2>
+</div>
+
+)
+
+}
+
+
+export default home;
---
+++ src/component/welcome.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+function welcome () {
+
+return(
+
+    <div>
+        
+        <h1>welcome to our website</h1>
+
+    </div>
+)
+
+}
+
+export default welcome;
+
